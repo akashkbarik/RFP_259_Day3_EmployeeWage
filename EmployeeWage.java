0933d2e7b64ac23ba65b2669741c6095368b00d5
akashkbarik <akashkbarik12@gmail.com>
2023-02-22 23:55:42 +0530

    Added: usecase2(UC2) to the program

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,14 +5,17 @@ public class EmployeeWage {
         System.out.println("welcome to the EmployeeWage program\n...............................");
         //Constant
         int IS_PRESENT = 1;
-        //generating random numbers 10 times as 0 or 1.
+        int SALARY_PER_HOUR = 20;
         double empCheck = Math.floor(Math.random() * 10) % 2;
+        int workingHour = 0;
         System.out.println(empCheck);
-        //checking if employee is present or not.
-        if (empCheck == IS_PRESENT)
-            System.out.println("Employee is Present");
-        else
-            System.out.println("Employee is Absent");
+
+        if (empCheck == IS_PRESENT) {
+            workingHour = 8;
+        }
+        double empSalary = workingHour * SALARY_PER_HOUR;
+
+        System.out.println("Employee has earned " + empSalary + " dollar today");
     }
 }
 
