e0d1a122ae7a821a398de6595df685d9b4c329d0
akashkbarik <akashkbarik12@gmail.com>
2023-02-22 23:46:59 +0530

    Added: welcome program

---
+++ EmployeeWage.java
@@ -0,0 +1,9 @@
+package day3.assignment.employeewage;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println("welcome to the EmployeeWage program\n...............................");
+
+    }
+}
+
